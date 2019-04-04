7613f85559715877be2dacbb22c199726a85f8cf
Tendai Chikake <tendaichikae@gmail.com>
2019-04-04 23:24:42 +0300

    Updates

---
+++ precache-manifest.9cf7bb1dd6b4f983c8d90b6287e93d0a.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = [
+  {
+    "revision": "efdf142fdc964d6575cf",
+    "url": "/virtual-dom/static/js/main.1635a1f2.chunk.js"
+  },
+  {
+    "revision": "93fb6ee0910baefa8217",
+    "url": "/virtual-dom/static/js/runtime~main.a26b8628.js"
+  },
+  {
+    "revision": "709b2aacb520678f4afe462aa59a3769",
+    "url": "/virtual-dom/index.html"
+  }
+];
